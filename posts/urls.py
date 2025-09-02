77243a1fdd8ba1a3fb7eb6686602a0935cd6802f
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-09-02 10:51:59 +0100

    new

--- posts/urls.py
+++ posts/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.login, name='login'),
     path('post/<str:pk>', views.post, name='post'),
-    path('createpost', views.createpost, name='createpost')
+    path('createpost', views.createpost, name='createpost'),
+    path('index', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout'),
+    path('login', views.login, name='login')
+
+
     
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Posts
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+
+
 
 # Create your views here.
 def index(request):
@@ -11,7 +16,7 @@ def post(request, pk):
     return render(request, 'post.html', {'posts':posts})
 
 def createpost(request):
-    if request. method == 'POST':
+    if request.method == 'POST':
         title = request.POST['title']
         newpost = request.POST['newpost']
         post_created = Posts(title=title, body=newpost)
@@ -20,3 +25,55 @@ def createpost(request):
     else:
         # If the request method is GET, render the create post form   
         return render(request, 'createpost.html')
+    
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        try:
+            user_obj = User.objects.get(email=email)
+            user = auth.authenticate(username=user_obj.username, password=password)
+        except User.DoesNotExist:
+            user = None
+
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+    
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        email = request.POST['email']
+
+        if password1==password2:
+                
+
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username exists')
+                return redirect('register')
+            elif User.objects.filter(email=email):
+                messages.info(request, 'Email exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password1, email=email)
+                user.save()
+        else:
+            messages.info(request, 'Password unmatch')
+            return redirect('register')
+    
+    return render(request, 'register.html')
